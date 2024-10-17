0ac38d5f626b3247e60144c425d884629c28e0f8
Alex <alexandrepinto586@gmail.com>
2024-10-17 14:21:14 -0300

    initial perceptions of regression model

--- src/classification.py
+++ src/classification.py
@@ -1,2 +1,10 @@
+import numpy as np
+
+
 class Classification:
+    def __init__(self, data):
+        self.data = data
+
+
+if __name__ == '__main__':
     pass
--- src/regression.py
+++ src/regression.py
@@ -1,2 +1,37 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
 class Regression:
-    pass
+    def __init__(self, file_name: str):
+        self.data = np.loadtxt(file_name)
+        self.N = self.data.shape[0]
+        self.x = self.data[:, 0]
+        self.x.shape = (self.N, 1)
+        self.y = self.data[:, 1]
+        self.y.shape = (self.N, 1)
+        self.w = self.define_weights()
+
+    def define_weights(self):
+        X = np.concatenate(
+            (
+                np.ones((self.N, 1)),
+                self.x),
+            axis=1
+        )
+        return np.linalg.pinv(X.T@X)@X.T@self.y
+
+    def function_label(self):
+        pass
+
+    def show_graphic(self):
+        fig, ax = plt.subplots()
+        ax.scatter(self.x, self.y, edgecolors='k')
+        plt.xlabel('Velocidade do vento')
+        plt.ylabel('Potência gerada pelo aerogerador')
+        plt.show()
+
+
+if __name__ == '__main__':
+    regression = Regression('aerogerador.dat')
+    regression.show_graphic()
